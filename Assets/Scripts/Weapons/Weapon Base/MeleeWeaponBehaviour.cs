a0780a985f11968b78d5a4c4027396d31cc82042
ThatNoodleGuy <nudeldor@gmail.com>
2026-05-04 12:20:16 +0300

    Add initial configuration files and player control assets
    - Created .vscode configuration files for project settings and recommended extensions.
    - Added ignore.conf to specify files and directories to be ignored by version control.
    - Introduced PlayerControls.inputactions and generated PlayerControls.cs for input handling.
    - Added various animation assets and metadata for player and enemy characters.
    - Established folder structure for Art, Animations, and Prefabs with corresponding metadata files.

---
+++ Assets/Scripts/Weapons/Weapon Base/MeleeWeaponBehaviour.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeaponBehaviour : MonoBehaviour
+{
+    [SerializeField] private float destroyAfterSeconds;
+
+    protected virtual void Start()
+    {
+        Destroy(gameObject, destroyAfterSeconds);
+    }
+}
